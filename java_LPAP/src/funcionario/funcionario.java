8db8eb732eadc7abed81983ca5f79104be9f1398
helovd <heloisavidaldourado@outlook.com>
2026-04-16 11:47:10 -0300

    Add FuncionarioPOO + iniciando funcionario

--- java_LPAP/src/funcionario/funcionario.java
+++ java_LPAP/src/funcionario/funcionario.java
@@ -1,5 +1,13 @@
 package funcionario;
 
-public class funcionario {
+import java.util.Scanner;
+import funcionario.funcionarioPoo;
 
+public class funcionario {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+
+
+}
 }
--- java_LPAP/src/funcionario/funcionarioPOO.java
+++ java_LPAP/src/funcionario/funcionarioPOO.java
@@ -1,5 +1,20 @@
 package funcionario;
 
-public class funcionarioPOO {
+import funcionario
 
+public class funcionarioPOO {
+    public String = nome;
+    public double = salarioBruto;
+    public double = tax;
+
+    public double totalS(){
+       double totalS;
+        return totalS = salarioBruto - tax;
+    }
+
+    public double totalPerc(){
+        double percent;
+        double totalPerc;
+        return totalPerc = salarioBruto + percent/100.00;
+    }
 }
